3b0ca487e6aafeba849e1dfe9ea284398eae4c17
Jayathunga T.M <thiroshmadhusha0520@gmail.com>
2023-05-30 13:28:42 +0530

    Center widget

--- basic_widgets/lib/main.dart
+++ basic_widgets/lib/main.dart
@@ -10,9 +10,46 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Flutter Widgets',
-      home: Scaffold()
-    );
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            // ************************************************
+            // Center Container BOX
+            height: 250,
+            width: 250,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(0),
+              color: Colors.red,
+            ),
+            // Text Center
+            child: const Center(child: Text("Hello World!")),
+            ),
+        ),
+
+          // Square Container BOX
+
+          // height: 100,
+          // width: 400,
+          // color: Colors.red,
+
+          // ************************************************
+
+          // Circle Container BOX
+
+          // height: 250,
+          // width: 250,
+          // decoration: BoxDecoration(
+          //   borderRadius: BorderRadius.circular(1000),
+          //   color: Colors.red,
+          // ),
+          // child: const Text("Hello World!"),
+
+          // ************************************************
+
+          
+        ),
+      );
   }
 }
